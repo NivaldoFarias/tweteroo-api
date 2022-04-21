d80324fb529c0616658d48f4567afd15e45b3b57
Nivaldo Farias <badivia.f@gmail.com>
2022-04-21 12:07:56 -0300

    Added json data files and app routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import chalk from "chalk";
+import * as fs from "fs";
+import os from "os";
 
 const app = express(),
-  port = process.env.PORT || 5000;
+  port = process.env.PORT || 5000,
+  USERS_PATH = "src/data/users.json",
+  TWEETS_PATH = "src/data/tweets.json";
 
 app.get("/", (req, res) => {
-  res.send(`Server is running on port ${port}`);
+  fs.readFile(USERS_PATH, (err, data) => {
+    if (err) throw err;
+    res.send(JSON.parse(data));
+  });
+  console.log(`Processing request from ${chalk.bold.red(req.ip)}`);
 });
 
 app.listen(port, () => {
